5560e2e6a9a060b2af10d884ea99abaf091c9ab0
ahmed alkasaby <168755711+ahmedAlkasaby@users.noreply.github.com>
2025-10-01 02:50:32 +0300

    create migration  user_codes

---
+++ database/migrations/2025_09_30_234743_create_user_codes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_codes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('phone', 191)->nullable();
+            $table->string('email', 191)->nullable();
+            $table->string('type', 50)->nullable();
+            $table->string('name_first', 191)->nullable();
+            $table->string('name_last', 191)->nullable();
+            $table->string('name', 191)->nullable();
+            $table->string('username', 191)->nullable();
+            $table->string('country_code', 191)->nullable();
+            $table->string('code', 191)->nullable();
+            $table->timestamp('code_expire')->nullable();
+            $table->timestamps();
+            $table->index('phone');
+            $table->index('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_codes');
+    }
+};
